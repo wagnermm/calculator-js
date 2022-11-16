af53005475457273131faec1e770999dbb7b787d
Wagner Miranda <wagnermarquesdemiranda@gmail.com>
2022-11-16 08:53:50 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const resultado = document.querySelector('result');
-const confirmar = document.querySelector('igual');
+const resultado = document.querySelector('.result');
+const confirmar = document.querySelector('.igual');
 
 function insert( valor ) {
     resultado.innerHTML += valor;
